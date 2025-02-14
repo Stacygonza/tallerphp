97ea5aaa410ed3e5d887308bbaafdd2192082289
unknown <stacygonzalez200620@gmail.com>
2025-02-13 21:27:13 -0500

    tallerphp

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+</head>
+<body>
+    <form action="recibe.php" name="formulario_contacto" method="post">
+        <input type="text" placeholder="Nombre:" name="nombre" id="nombre">
+        <input type="text" placeholder="Edad:" name="edad" id="edad">
+        <br>
+
+<!-- Radio Button -->
+        <label for="hombre">Hombre</label>
+        <input type="radio" name="sexo" id="hombre" value="hombre">
+        <br>
+
+        <label for="mujer">Mujer</label>
+        <input type="radio" name="sexo" id="mujer" value="mujer">
+        <br>
+
+<!-- Combobox -->
+        <select name="year" id="year">
+            <option value="2000">2000</option>
+            <option value="2001">2001</option>
+            <option value="2002">2002</option>
+        </select>
+        <br>
+
+<!-- Checkbox --> 
+        <label for="terminos">Terminos y Condiciones</label>
+        <input type="checkbox" name="terminos" value="ok" id="terminos">
+        <br>
+
+        <input type="submit" name="btn-enviar" value="Enviar">
+    </form>
+</body>
+</html>
